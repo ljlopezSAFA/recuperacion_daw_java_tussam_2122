bc5372f8721dffcd6177231bdc3ddf1d6924324d
ljlopezSAFA <94695938+ljlopezSAFA@users.noreply.github.com>
2024-03-05 20:06:18 +0100

    Update UtilidadesConductor.java

--- src/main/java/utilidades/UtilidadesConductor.java
+++ src/main/java/utilidades/UtilidadesConductor.java
@@ -25,17 +25,7 @@ public class UtilidadesConductor {
      * @return
      */
     public static JornadaLaboral terminarJornadaLaboral(Conductor conductor, Map<Autobus,Integer> autobuses_num_itinerarios){
-        JornadaLaboral jornadaLaboral = new JornadaLaboral();
-        jornadaLaboral.setId(1);
-        jornadaLaboral.setConductor(conductor);
-        jornadaLaboral.setAutobuses_num_itinerarios(autobuses_num_itinerarios);
-        jornadaLaboral.setNum_paradas_totales(autobuses_num_itinerarios.keySet()
-                .stream().mapToInt(a-> a.getRecorrido().getParadas().size()).sum());
-        jornadaLaboral.setFecha(LocalDate.now());
-        jornadaLaboral.setSalario(autobuses_num_itinerarios.keySet().stream()
-                .mapToDouble(a-> a.getRecorrido().getParadas()
-                        .stream().mapToDouble(Parada::getBonificacion_parada).sum() * autobuses_num_itinerarios.get(a)).sum());
-        return  jornadaLaboral;
+        return null;
     }
 
 
@@ -50,23 +40,7 @@ public class UtilidadesConductor {
      */
     public static Parada getParadaPosible(Double importe_restante, List<Autobus> autobuses){
 
-        Parada paradaPosible = null;
-        List<Parada> paradasARecorrer = new ArrayList<>();
-        autobuses.forEach(a-> paradasARecorrer.addAll(a.getRecorrido().getParadas()));
-
-        for(Parada p : paradasARecorrer){
-            if(importe_restante - p.getBonificacion_parada() > 0.0){
-                paradaPosible = p;
-            }
-            importe_restante -= p.getBonificacion_parada();
-
-            if(importe_restante< 0.0){
-                break;
-            }
-
-        }
-
-        return  paradaPosible;
+       return null;
     }
 
 
